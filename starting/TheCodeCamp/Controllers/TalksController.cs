2e56798bf9d2c8895d220dba79f42bf7045b09d9
Srivatsa Haridas <srivatsa.haridas@honeywell.com>
2019-05-03 09:52:15 +0530

    Feature API Association, Sample is modified to include the Talks related to the Camps

---
+++ starting/TheCodeCamp/Controllers/TalksController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TheCodeCamp.Data;
+using TheCodeCamp.Models;
+
+namespace TheCodeCamp.Controllers
+{
+    [RoutePrefix("api/camps/{moniker}/talks")]
+    public class TalksController : ApiController
+    {
+        private readonly ICampRepository campRepository;
+        private readonly IMapper mapper;
+
+        public TalksController(ICampRepository campRepository, IMapper _mapper)
+        {
+            this.campRepository = campRepository;
+            this.mapper = _mapper;
+        }
+
+        [Route()]
+        public async Task<IHttpActionResult> Get(string moniker, bool includeSpeakers = false)
+        {
+            try
+            {
+                var results = await campRepository.GetTalksByMonikerAsync(moniker,includeSpeakers);
+
+                return Ok(mapper.Map<IEnumerable<TalkModel>>(results));
+
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
