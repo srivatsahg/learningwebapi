e2d2a43a558bd978f0d76eb66967bd20d7ffcfa3
Srivatsa Haridas <srivatsa.haridas@honeywell.com>
2019-05-06 12:39:28 +0530

    Introduce API versioning - Default versioning information specified - Using the AssumeDefaultversionWhenUnspecified property to assign a default version of the API -ReportAPIversion returns the supported version in the response header (To be verified in the POSTMAN)

--- starting/TheCodeCamp/App_Start/WebApiConfig.cs
+++ starting/TheCodeCamp/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json.Serialization;
+﻿using Microsoft.Web.Http;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,15 @@ namespace TheCodeCamp
     {
       // Web API configuration and services
       AutofacConfig.Register();
+
+            //API Versioning
+            config.AddApiVersioning(cfg =>
+            {
+                cfg.DefaultApiVersion = new ApiVersion(1, 1);
+                //so that user does not specify in the url
+                cfg.AssumeDefaultVersionWhenUnspecified = true;
+                cfg.ReportApiVersions = true;
+            });
 
             config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
                       new CamelCasePropertyNamesContractResolver();
