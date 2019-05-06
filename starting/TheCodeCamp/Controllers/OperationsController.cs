6c46224dcedd4deec55f76670f7a02e835c9ab98
Srivatsa Haridas <srivatsa.haridas@honeywell.com>
2019-05-06 11:32:52 +0530

    Functional APIs implementation sample

---
+++ starting/TheCodeCamp/Controllers/OperationsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace TheCodeCamp.Controllers
+{
+    /// <summary>
+    /// A simple example to demonstrate Functional APIs 
+    /// Example:
+    /// APIs are not just for exchanging of data. 
+    /// They provide much needed functionality for developers and integrators. 
+    /// The functionality of an API can provide shipping costs as the Fedex API does, 
+    /// or provide you with directions from San Francisco, CA to New York, NY as with the Google Maps API.
+    /// 
+    /// In this simple example, the RefreshConfiguration call refreshes the Web configuration file for any changes
+    /// </summary>
+    public class OperationsController : ApiController
+    {
+        [HttpOptions]
+        [Route("api/refreshConfig")]
+        public IHttpActionResult RefreshConfiguration()
+        {
+            try
+            {
+                ConfigurationManager.RefreshSection("appSettings");
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
